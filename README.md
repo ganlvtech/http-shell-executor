b64dd86f30e0e9effe2a2ed56ffc5f9cdca6a70a
Ganlv <ganlvtech@qq.com>
2025-03-29 02:52:05 +0800

    Add upload routes. Improve README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # HTTP Shell Executor
 
-A simple server serving shell script. You can use it as a simple script based CI/CD server.
+A simple server serving shell script. It can act like a simple script based CI/CD server.
 
 ## Usage
 
@@ -9,6 +9,8 @@ A simple server serving shell script. You can use it as a simple script based CI
 ```
 
 ```bash
+./http-shell-executor
+./http-shell-executor --upload
 ./http-shell-executor --listen 0.0.0.0:8000
 ```
 
@@ -16,24 +18,34 @@ A simple server serving shell script. You can use it as a simple script based CI
 
 With the default configuration, static files are served from `./public/` and serving at `/` using GET method, and dynamic scripts are served from `./scripts/` and serving at `/` with POST method.
 
-Static files are serving with `Content-Type` header which is detected by file extension. Dynamic scripts' headers are always `Content-Type: text/event-stream; charset=utf-8`.
+Static files are serving with `Content-Type` header which is detected by file extension. Dynamic scripts' headers are always `Content-Type: text/plain; charset=utf-8` abd `X-Content-Type-Options: nosniff`.
 
-Streaming support. If your script not finished, it would convert to a streaming mode.
+Streaming response is supported for scripts. Script response is always trunked. It would be continued until the script exits or the connection is closed. If the script exits, the response will stop and the connection will be closed. If the connection is closed, the script will receive a `SIGTERM` signal.
 
 Generated homepage not support. Please use static homepage + Fetch API.
 
+Upload is supported. You need to add `--upload` in command line. You can PUT file to `./public/uploads/` dir via `/uploads/` path. The file will be overwritten if it exists.
+
 ## Scripting
 
-Script must start with [shebang](https://en.wikipedia.org/wiki/Shebang_(Unix)). You can also use an binary executable.
+Script must start with [`#!`](https://en.wikipedia.org/wiki/Shebang_(Unix)). You can also use a binary executable. Scripts and executables need to be `chmod +x`.
 
-## Example
+script arguments are passed as query string and `application/x-www-form-urlencoded` body. every `args=value` and `args[]=value` will be considered as an argument.
 
-example can be found in `public/` and `scripts/` dir.
+You may love [jq](https://github.com/stedolan/jq) for script programming.
 
-### Run
+## Examples
+
+examples can be found in `public/` and `scripts/` dir.
+
+## Build
+
+Build `./target/x86_64-unknown-linux-musl/release/http-shell-executor` with [muslrust](https://hub.docker.com/r/clux/muslrust/).
 
 ```bash
-./http-shell-executor
+docker run -it --rm -v cargo-cache:/root/.cargo/registry -v "$(pwd)":/volume clux/muslrust:stable cargo build --release
 ```
 
-You may like [jq](https://github.com/stedolan/jq).
+## License
+
+[MIT License](https://mit-license.org/license.txt)
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use axum::extract::{OriginalUri, Query};
+use axum::extract::{DefaultBodyLimit, OriginalUri, Query};
 use axum::http::{Response, StatusCode};
 use axum::response::IntoResponse;
-use axum::routing::post;
-use axum::{Extension, Form, Router};
-use bytes::Bytes;
+use axum::{Extension, Form};
+use bytes::{Bytes};
 use clap::Parser;
 use http_body::Frame;
 use std::path::PathBuf;
@@ -11,13 +10,16 @@ use std::pin::Pin;
 use std::process::Stdio;
 use std::sync::Arc;
 use std::task::{Context, Poll};
-use tokio::io::{AsyncRead, ReadBuf};
+use axum::routing::{post, put};
+use tokio::fs::{create_dir_all, File};
+use tokio::io::{AsyncRead, AsyncWriteExt, ReadBuf};
 use tokio::process::Command;
 use tower_http::services::{ServeDir, ServeFile};
 
 pub fn path_clean(path: impl AsRef<str>) -> String {
     let mut out = Vec::<&str>::new();
-    for comp in path.as_ref().split("/") {
+    let path = path.as_ref().replace("\\", "/");
+    for comp in path.split("/") {
         match comp {
             "" => (),
             "." => (),
@@ -32,39 +34,26 @@ pub fn path_clean(path: impl AsRef<str>) -> String {
 
 #[derive(Debug)]
 struct OwnedResourceStream<T, R> {
+    name: String,
     _resource: T,
     reader: R,
 }
 
 impl<T, R> OwnedResourceStream<T, R> {
-    pub fn new(resource: T, reader: R) -> Self {
+    pub fn new(name: String, resource: T, reader: R) -> Self {
         OwnedResourceStream {
+            name,
             _resource: resource,
             reader,
         }
     }
 }
 
-// impl<T, R: AsyncRead> Stream for OwnedResourceStream<T, R> {
-//     type Item = Result<Bytes, std::io::Error>;
-//
-//     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-//         let mut buf = BytesMut::with_capacity(4096);
-//         let reader = self.project().reader;
-//         let mut reader = Pin::new(reader);
-//         match reader.poll_read(cx, &mut buf) {
-//             Poll::Ready(Ok(())) => {
-//                 if buf.is_empty() {
-//                     Poll::Ready(None)
-//                 } else {
-//                     Poll::Ready(Some(Ok(buf.freeze())))
-//                 }
-//             }
-//             Poll::Ready(Err(e)) => Poll::Ready(Some(Err(e))),
-//             Poll::Pending => Poll::Pending,
-//         }
-//     }
-// }
+impl<T, R> Drop for OwnedResourceStream<T, R> {
+    fn drop(&mut self) {
+        eprintln!("OwnedResourceStream {} dropped", self.name);
+    }
+}
 
 impl<T: Unpin, R: AsyncRead + Unpin> http_body::Body for OwnedResourceStream<T, R> {
     type Data = Bytes;
@@ -90,77 +79,115 @@ impl<T: Unpin, R: AsyncRead + Unpin> http_body::Body for OwnedResourceStream<T, 
     }
 }
 
-struct AppConfig {
-    scripts_path: PathBuf,
-}
-
-async fn scripts(Extension(config): Extension<Arc<AppConfig>>, OriginalUri(original_uri): OriginalUri, Query(query_params): Query<Vec<(String, String)>>, Form(form_params): Form<Vec<(String, String)>>) -> impl IntoResponse {
-    let args: Vec<String> = query_params.iter().chain(form_params.iter())
+async fn handle_scripts(Extension(config): Extension<Arc<Args>>, OriginalUri(original_uri): OriginalUri, Query(query_params): Query<Vec<(String, String)>>, Form(form_params): Form<Vec<(String, String)>>) -> impl IntoResponse {
+    let path = match urlencoding::decode(original_uri.path()) {
+        Ok(path) => path,
+        Err(e) => {
+            eprintln!("Invalid path: {}", e);
+            return (StatusCode::BAD_REQUEST, format!("Invalid path: {}", e)).into_response();
+        }
+    };
+    #[allow(unused_mut)]
+    let mut args: Vec<String> = query_params.iter().chain(form_params.iter())
         .filter(|(k, _)| k == "args[]" || k == "args")
         .map(|(_, v)| v.to_owned())
         .collect();
-    let clean_path = path_clean(original_uri.path().replace("\\", "/"));
-    let script_path = config.scripts_path.join(&clean_path);
-    println!("Clean Path: {} Script Path: {} args: {:?}", &clean_path, script_path.to_string_lossy(), args);
+    let clean_path = path_clean(path);
+    #[allow(unused_mut)]
+    let mut script_path = PathBuf::from(&config.scripts_dir).join(&clean_path);
+    eprintln!("Clean Path: {} Script Path: {} args: {:?}", &clean_path, script_path.to_string_lossy(), args);
+    if !script_path.exists() {
+        return Response::builder().status(StatusCode::NOT_FOUND)
+            .header("Content-Type", "text/plain")
+            .body("Not Found".to_string())
+            .unwrap().into_response();
+    }
+    #[cfg(windows)]
+    if script_path.to_string_lossy().ends_with(".sh") {
+        let bash_path = PathBuf::from("C:\\Program Files\\Git\\bin\\bash.exe");
+        if bash_path.exists() {
+            args.insert(0, script_path.to_string_lossy().into_owned());
+            script_path = bash_path;
+        }
+    }
 
+    let name = format!("{} {:?}", script_path.to_string_lossy(), args);
     let mut cmd = Command::new(script_path);
     cmd.args(args);
     cmd.stdin(Stdio::null());
     cmd.stdout(Stdio::piped());
-    cmd.stderr(Stdio::null());
+    cmd.stderr(Stdio::inherit());
     cmd.kill_on_drop(true);
     let mut child = match cmd.spawn() {
         Ok(child) => child,
         Err(e) => {
-            println!("Spawn cmd error: {:?}. Error: {:?}", cmd, e);
-            return Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR)
-                .header("Content-Type", "text/plain")
-                .body("Spawn cmd error".to_string())
-                .unwrap().into_response();
+            eprintln!("Spawn cmd error: {:?}. Error: {:?}", cmd, e);
+            return (StatusCode::INTERNAL_SERVER_ERROR, "Spawn cmd error").into_response();
         }
     };
     let stdout = match child.stdout.take() {
         Some(stdout) => stdout,
         None => {
-            println!("Child did not have a handle to stdout: {:?}", cmd);
-            return Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR)
-                .header("Content-Type", "text/plain")
-                .body("Child did not have a handle to stdout".to_string())
-                .unwrap().into_response();
+            eprintln!("Child did not have a handle to stdout: {:?}", cmd);
+            return (StatusCode::INTERNAL_SERVER_ERROR, "Child did not have a handle to stdout").into_response();
         }
     };
-    let owned_stream = OwnedResourceStream::new(child, stdout);
+    let owned_stream = OwnedResourceStream::new(name, child, stdout);
     Response::builder()
         .status(StatusCode::OK)
-        .header("Content-Type", "text/plain")
+        .header("Content-Type", "text/plain; charset=utf-8")
         .header("X-Content-Type-Options", "nosniff")
         .body(owned_stream)
         .unwrap().into_response()
 }
 
-#[derive(Parser, Debug)]
+async fn handle_upload(Extension(config): Extension<Arc<Args>>, OriginalUri(original_uri): OriginalUri, body: Bytes) -> Result<(), (StatusCode, String)> {
+    if !config.upload {
+        return Err((StatusCode::FORBIDDEN, "Upload is disabled".to_string()));
+    }
+    let path = urlencoding::decode(original_uri.path()).map_err(|e| (StatusCode::BAD_REQUEST, format!("Invalid path: {}", e)))?;
+    let path = match path.strip_prefix(&config.upload_path_prefix) {
+        Some(path) => path,
+        None => return Err((StatusCode::FORBIDDEN, format!("Invalid path: {}", path))),
+    };
+    let full_path = PathBuf::from(&config.upload_dir).join(path_clean(path));
+    eprintln!("Upload {} bytes to: {:?}", body.len(), full_path);
+    if let Some(parent) = full_path.parent() {
+        create_dir_all(parent).await.map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, format!("Failed to create directories: {}", e)))?;
+    }
+    File::create(&full_path).await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, format!("Failed to create file: {}", e)))?
+        .write_all(&body).await
+        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, format!("Failed to write file: {}", e)))?;
+    Ok(())
+}
+
+#[derive(Parser, Clone, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long, value_name = "IP:PORT", help = "Listening IP and port", default_value = "127.0.0.1:80")]
     listen: String,
     #[arg(long, value_name = "DIR", help = "Static files directory path", default_value = "./public/")]
-    public: String,
+    public_dir: String,
     #[arg(long, value_name = "FILE", help = "Index filename", default_value = "./public/index.html")]
     public_index: String,
     #[arg(long, value_name = "DIR", help = "Shell scripts directory path", default_value = "./scripts/")]
-    scripts: String,
+    scripts_dir: String,
+    #[arg(long, help = "Enable file upload", default_value = "false")]
+    upload: bool,
+    #[arg(long, value_name = "DIR", help = "Upload directory path", default_value = "./public/uploads")]
+    upload_dir: String,
+    #[arg(long, value_name = "PREFIX", help = "Upload URL path prefix", default_value = "/uploads")]
+    upload_path_prefix: String,
 }
 
 #[tokio::main]
 async fn main() {
     let config = Args::parse();
-    let app_config = Arc::new(AppConfig {
-        scripts_path: PathBuf::from(config.scripts),
-    });
-    let app = Router::new()
-        .route("/{*path}", post(scripts))
-        .layer(Extension(app_config))
-        .fallback_service(ServeDir::new(&config.public).fallback(ServeFile::new(&config.public_index)));
+    let app = post(handle_scripts)
+        .merge(put(handle_upload).layer(DefaultBodyLimit::max(200 * 1024 * 1024)))
+        .fallback_service(ServeDir::new(&config.public_dir).fallback(ServeFile::new(&config.public_index)))
+        .layer(Extension(Arc::new(config.clone())));
 
     let listener = tokio::net::TcpListener::bind(&config.listen).await.unwrap();
     println!("Server is listening on {}", &config.listen);
